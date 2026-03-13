eea942e1fa46b0d086132f5e7497168c9256897c
David Shuma <tdshuma@outlook.com>
2026-03-13 11:49:03 +0200

    feat: add dashboard

---
+++ app/src/Fastway/Dashboard/Controller/DashboardController.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fastway\Dashboard\Controller;
+
+use Laminas\Diactoros\Response;
+use League\Plates\Engine;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class DashboardController
+{
+    public function __invoke(ServerRequestInterface $request): ResponseInterface
+    {
+        $templates = new Engine(__DIR__ . '/../View');
+        $response = new Response();
+
+        $response->getBody()->write($templates->render('dashboard', ['name' => 'David']));
+        return $response;
+    }
+}
---
+++ app/src/Fastway/Dashboard/View/dashboard.php
@@ -0,0 +1,4 @@
+<?php $this->layout('template', ['title' => 'Dashboard']) ?>
+
+<h1>Dashboard</h1>
+<p>Hello, <?=$this->e($name)?></p>
